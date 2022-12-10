580786c91c9bf326e97dcdd662c3a86a88904272
Jutipong.Dev <techcom.ec20@gmail.com>
2022-12-11 01:39:13 +0700

    change: package

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex">
     <div class="m-auto mt-52">
-      <h1 class="text-6xl text-blue-500">Welcome to Nuxt3 course</h1>
+      <h1 class="text-6xl text-blue-500">Welcome to Nuxt3</h1>
       <h2>Counter: {{ counter.counter }}</h2>
       <button class="bg-blue-500 p-4 m-2" @click="counter.increment">+</button>
       <button class="bg-red-500 p-4 m-2" @click="counter.decrement">-</button>
