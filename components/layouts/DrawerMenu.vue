54494250b8d56d36d3c5fe45fc94456b03356853
Jutipong.Dev <techcom.ec20@gmail.com>
2022-12-12 20:35:40 +0700

    update code

--- components/layouts/DrawerMenu.vue
+++ components/layouts/DrawerMenu.vue
@@ -9,24 +9,42 @@
     </div>
   </q-img>
   <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px">
+    <!-- home -->
     <q-list>
-      <q-expansion-item expand-separator label="Account settings">
-        <q-item clickable v-ripple :inset-level="0.2">
+      <q-item :focused="false" clickable class="hover:text-sky-600 hover:font-bold hover:bg-opacity-100">
+        <q-item-section avatar>
+          <q-icon name="outlined" />
+        </q-item-section>
+        <q-item-section> label </q-item-section>
+      </q-item>
+    </q-list>
+    <!-- end home -->
+    <q-list>
+      <q-expansion-item expand-separator label="Menu 1">
+        <q-item clickable :inset-level="0.2" class="hover:text-sky-600 hover:font-bold">
           <q-item-section avatar>
             <q-icon name="home" />
           </q-item-section>
-          <q-item-section> label </q-item-section>
+          <q-item-section> label 1.1</q-item-section>
         </q-item>
       </q-expansion-item>
     </q-list>
 
     <q-list>
-      <q-expansion-item expand-separator label="Account settings" v-model="root">
-        <q-item clickable v-ripple :inset-level="0.2">
+      <q-expansion-item expand-separator label="Menu 2" v-model="root">
+        <!-- item -->
+        <q-item clickable :inset-level="0.2" class="hover:text-sky-600 hover:font-bold">
           <q-item-section avatar>
             <q-icon name="home" />
           </q-item-section>
-          <q-item-section> label </q-item-section>
+          <q-item-section> 2.1</q-item-section>
+        </q-item>
+        <!-- item 2  -->
+        <q-item clickable :inset-level="0.2" class="hover:text-sky-600 hover:font-bold">
+          <q-item-section avatar>
+            <q-icon name="home" />
+          </q-item-section>
+          <q-item-section> 2.2</q-item-section>
         </q-item>
       </q-expansion-item>
     </q-list>
